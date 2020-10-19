feb2dc0f43f8e7fbf80980853993ffc618be731c
sfinim <sfinim.music@gmail.com>
2020-10-19 12:07:42 +0900

    yolo4/yolo4.py

--- yolo4/yolo4.py
+++ yolo4/yolo4.py
@@ -3,6 +3,11 @@ from PIL import Image
 
 IMAGE_PATH = "/tmp/data.jpg"
 
+dict_label = {0:'hey-tea', 
+              1:'coca-cola',
+              2:'grapefruit-squash',
+              3:'gogo-tea',
+              4:'pocari'}
 
 def detect_img(yolo):
     image, bboxes = yolo.inference(IMAGE_PATH)
@@ -18,7 +23,24 @@ def detect_img(yolo):
             r_scores.append(bbox[-1])
         
     for i, score in enumerate(r_scores):
-        if score <= 0.8:
+        flg_None = 0
+        if dict_label[r_classes[i]] == 'hey-tea':
+            if score <= 0.63:
+                flg_None = 1
+        elif dict_label[r_classes[i]] == 'coca-cola':
+            if score <= 0.8:
+                flg_None = 1
+        elif dict_label[r_classes[i]] == 'grapefruit-squash':
+            if score <= 0.93:
+                flg_None = 1
+        elif dict_label[r_classes[i]] == 'gogo-tea':
+            if score <= 0.79:
+                flg_None = 1
+        elif dict_label[r_classes[i]] == 'pocari':
+            if score <= 0.8:
+                flg_None = 1
+
+        if flg_None == 1:
             r_scores[i] = None
             r_classes[i] = None
 
